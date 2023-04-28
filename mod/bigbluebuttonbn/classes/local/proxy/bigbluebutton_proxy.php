55dd349a33282a8883ea7cb7971028d9be0af87c
Shamiso.Jaravaza <33659194+ssj365@users.noreply.github.com>
2023-04-28 09:53:49 -0600

    MDL-76880 mod_bigbluebuttonbn: Fix guest parameter

--- mod/bigbluebuttonbn/classes/local/proxy/bigbluebutton_proxy.php
+++ mod/bigbluebuttonbn/classes/local/proxy/bigbluebutton_proxy.php
@@ -105,11 +105,16 @@ class bigbluebutton_proxy extends proxy_base {
             'role' => $instance->get_current_user_role()
         ];
 
-        if (!$isguestjoin) {
-            $data['userID'] = $instance->get_user_id();
-            $data['guest'] = "false";
+        // Check if guest access enabled before setting guest parameter.
+        if ($instance->is_guest_allowed()) {
+            if (!$isguestjoin) {
+                $data['userID'] = $instance->get_user_id();
+                $data['guest'] = "false";
+            } else {
+                $data['guest'] = "true";
+            }
         } else {
-            $data['guest'] = "true";
+            $data['userID'] = $instance->get_user_id();
         }
 
         if (!is_null($jointime)) {
