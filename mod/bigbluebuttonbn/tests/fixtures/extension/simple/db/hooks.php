89bf4bae754a964b497d836dd7004bccfb138641
Shamiso.Jaravaza <33659194+ssj365@users.noreply.github.com>
2025-07-03 15:12:19 -0600

    Added behat tests

---
+++ mod/bigbluebuttonbn/tests/fixtures/extension/simple/db/hooks.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Hook callbacks for BigBlueButton Simple for extending Settings Navigation
+ *
+ * @package   bbbext_simple
+ * @copyright 2025 Blindside Networks Inc
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$callbacks = [
+    [
+        'hook' => \mod_bigbluebuttonbn\hook\extend_settings_navigation_append::class,
+        'callback' => \bbbext_simple\hook\extend_settings_navigation_append::class . '::append_settings_navigation'
+    ],
+];
